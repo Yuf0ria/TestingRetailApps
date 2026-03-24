ff7176c45f3ee5d24f212f23270c32259f9b043f
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2026-03-24 14:33:48 +0800

    feat(auth): valid login

--- Tests/Authentication.js
+++ Tests/Authentication.js
@@ -5,10 +5,33 @@ async function authentication () {
   let driver = await new Builder().forBrowser('chrome').build()
 
   try {
-    await driver.get('http:/localhost/')
+    await driver.get(
+      'http://localhost/index.php?route=account/login&language=en-gb'
+    )
     await driver.manage().window().maximize()
+
+    await driver.findElement(By.id('input-email')).sendKeys('johndoe@email.com')
+    await driver.findElement(By.id('input-password')).sendKeys('eA07:?j6*&Kq')
+
+    await driver
+      .findElement(
+        By.xpath(
+          '/html/body/div/main/div[2]/div/div/div/div[2]/div/form/div[3]/button'
+        )
+      )
+      .click()
+
+    let element = await driver.findElement(
+      By.xpath('//*[contains(text(), "My Account")]')
+    )
+    let actualText = await element.getText()
+    let expectedText = 'My Account'
+
+    assert.strictEqual(actualText, expectedText, 'Text Mismatch!')
+
+    console.log('Assertion passed!')
   } finally {
-    //
+    await driver.quit()
   }
 }
 authentication()
